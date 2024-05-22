67f34eb8c32e0fe8f66add5b445a5244bf8044ea
Alexander Kisliy <108825919+SashaKisliy@users.noreply.github.com>
2024-05-22 14:07:36 +0300

    complete settings and added admin

--- todo_list/admin.py
+++ todo_list/admin.py
@@ -4,13 +4,13 @@ from todo_list.models import Task, Tag
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ("content", "created_at", "deadline", 'done')
-    list_filter = ('done', 'created_at', 'deadline')
-    search_fields = ('content',)
-    ordering = ('done', '-created_at')
+    list_display = ("content", "created_at", "deadline", "done")
+    list_filter = ("done", "created_at", "deadline")
+    search_fields = ("content",)
+    ordering = ("done", "-created_at")
 
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-    search_fields = ('name',)
+    list_display = ("name",)
+    search_fields = ("name",)
