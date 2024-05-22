b541ea3074621954a5d300f434bae308cc0d9a8e
Alexander Kisliy <108825919+SashaKisliy@users.noreply.github.com>
2024-05-22 11:21:06 +0300

    add to admin fields new models

--- todo_list/admin.py
+++ todo_list/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from todo_list.models import Task, Tag
 
-# Register your models here.
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("content", "created_at", "deadline", 'done')
+    list_filter = ('done', 'created_at', 'deadline')
+    search_fields = ('content',)
+    ordering = ('done', '-created_at')
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
